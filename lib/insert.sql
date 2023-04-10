7b1e5324e9f931164dac8e1df44c31eaeff7d357
Connor Kormos <connorkormos97@gmail.com>
2023-04-10 10:07:14 -0700

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+("Mr. Chocolate", 5, "M", "blue", "chill", TRUE),
+("Rowdy", 4, "F", "red", "cool", TRUE),
+("Tabitha", 10, "M", "blue", "chill", TRUE),
+("Sergeant Brown", 26, "M", "pink", "rad", TRUE),
+("Melissa", 30, "M", "green", "cool", TRUE),
+("Grinch", 100, "M", "blue", "smart", FALSE),
+("Wendy", 60, "M", "blue", "smart", TRUE),
+(NULL, 90, "F", "purple", "loving", FALSE);
